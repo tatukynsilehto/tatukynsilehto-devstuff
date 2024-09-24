6e5c7118d347c5aad98c652a02b95a7caf29d8d4
Tatu Kynsilehto <tatu.kynsilehto@hotmail.com>
2024-09-24 09:09:27 +0300

    Create basic front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>hello world</h1>
+        <div className="button-container">
+          <button className="app-button" onClick={() => alert('App 1')}>
+            Open App 1
+          </button>
+          <button className="app-button" onClick={() => alert('App 2')}>
+            Open App 2
+          </button>
+          <button className="app-button" onClick={() => alert('App 3')}>
+            Open App 3
+          </button>
+          <button className="app-button" onClick={() => alert('App 4')}>
+            Open App 4
+          </button>
+        </div>
       </header>
     </div>
   );
